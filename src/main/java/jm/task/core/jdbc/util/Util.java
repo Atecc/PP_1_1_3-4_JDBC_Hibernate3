f2033bc19c038a07e95ed26b46093b0a222a8bac
Atecc <rizvan1127@gmail.com>
2023-11-01 15:27:55 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -44,19 +44,6 @@ public class Util {
         return sessionFactory;
     }
 }
-//    private static final String DB_URL = "jdbc:mysql://localhost:3306/baseone";
-//    private static final String DB_USERNAME = "root";
-//    private static final String DB_PASSWORD = "1234";
-//    public static Connection connection;
-//
-//    public static Connection getConnection() {
-//        try {
-//            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-//        } catch (SQLException e) {
-//            System.out.println("Произошла ошибка соединения!");
-//        }
-//        return connection;
-//    }
 
 
 
